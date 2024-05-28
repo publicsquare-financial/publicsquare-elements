96c771bc72122f887067f8c48c6f2db95b2c448d
rlfrahm <frahmryan@gmail.com>
2024-05-28 12:49:41 -0500

    chore: fix yet another mistake

--- playwright.config.ts
+++ playwright.config.ts
@@ -70,7 +70,7 @@ export default defineConfig({
 
   /* Run your local dev server before starting the tests */
   webServer: {
-    command: 'cd example-app && bun run dev',
+    command: 'cd example-app && yarn dev',
     url: 'http://127.0.0.1:3000',
     reuseExistingServer: !process.env.CI,
   },
